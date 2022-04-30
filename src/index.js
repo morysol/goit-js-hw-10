da6da846ec6e85482562d3e4215451a0ca713696
morysol <cepera.cMupHoB@gmail.com>
2022-04-30 22:22:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import countryListTpl from '../templates/countries-list.hbs';
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-aio-3.2.5.min.js';
 
-//
-// var debounce = require('lodash.debounce');
 import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
@@ -15,6 +13,7 @@ const searchBoxUI = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
+//
 const debouncedFn = debounce(onTextInput, DEBOUNCE_DELAY);
 
 searchBoxUI.addEventListener('input', event => {
@@ -22,67 +21,91 @@ searchBoxUI.addEventListener('input', event => {
 });
 
 function onTextInput(searchPattern) {
-  // console.log(searchPattern);
-
-  const url = `https://restcountries.com/v3.1/name/${searchPattern.trim()}?fields=name,capital,population,flags,languages`;
+  if (searchPattern === '') {
+    clearCountryCard();
+    clearCountryList();
+    return;
+  }
+  const baseURL = 'https://restcountries.com/v3.1/';
+  const fields = 'name,capital,population,flags,languages';
+  const url = `${baseURL}name/${searchPattern.trim()}?fields=${fields}`;
 
   fetch(url)
     .then(response => {
       if (!response.ok || response.status === 404) {
-        console.clear();
         throw new Error(response.status);
       }
       return response.json();
     })
-    .then(data => {
-      // console.log('---------', data);
-      // console.log('+++++++', data.length);
-      if (data.length === 1) {
+    .then(fetchedCountries => {
+      if (fetchedCountries.length === 1) {
         const {
           name: { official: country },
-          capital,
+          capital: countryCapital = capital.join(''),
           population,
           flags: { svg: flag },
-          languages,
-        } = data[0];
+          languages: countryLanguages = Object.values(languages).join(''),
+        } = fetchedCountries[0];
 
-        const countryCapital = capital.join('');
-        const countryLanguages = Object.values(languages).join('');
-        // console.log(country, countryCapital, population, flag, countryLanguages);
-
-        const countryCardMarkup = countryCardTpl({
+        const countryCardMarkup = makeCountryCardMarkup({
           country,
           countryCapital,
           population,
           flag,
           countryLanguages,
         });
-        renderCountryCard(countryCardMarkup);
-      } else if (data.length > 1 && data.length < 11) {
-        const countryListMarkup = ''.concat(
-          '<ul class="countries__list">',
-          data
-            .map(data =>
-              countryListTpl({
-                flag: data.flags.svg,
-                country: data.name.official,
-              }),
-            )
-            .join(''),
-          '</ul>',
-        );
 
+        clearCountryList();
+        renderCountryCard(countryCardMarkup);
+      } else if (fetchedCountries.length > 1 && fetchedCountries.length < 11) {
+        const countryListMarkup = makecountryListMarkup(fetchedCountries);
+
+        clearCountryCard();
         renderCountryList(countryListMarkup);
       } else {
-        Notiflix.Notify.success('Too many matches found. Please enter a more specific name.');
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
       }
     })
-    .catch(error => Notiflix.Notify.failure('Oops, there is no country with that name'));
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      Notiflix.Notify.failure(`${error}`);
+    });
 }
 
 function renderCountryCard(markup) {
   countryInfo.innerHTML = markup;
 }
+function clearCountryCard() {
+  countryInfo.innerHTML = '';
+}
 function renderCountryList(markup) {
   countryList.innerHTML = markup;
+}
+function clearCountryList() {
+  countryList.innerHTML = '';
+}
+
+function makeCountryCardMarkup({ country, countryCapital, population, flag, countryLanguages }) {
+  return countryCardTpl({
+    country,
+    countryCapital,
+    population,
+    flag,
+    countryLanguages,
+  });
+}
+
+function makecountryListMarkup(countryList) {
+  return ''.concat(
+    '<ul class="countries__list">',
+    countryList
+      .map(countryList =>
+        countryListTpl({
+          flag: countryList.flags.svg,
+          country: countryList.name.official,
+        }),
+      )
+      .join(''),
+    '</ul>',
+  );
 }
